cbef19b2d0622eacaf71ced945e6ca6f1ef84ca5
Bryce Kalow <bkalow@hashicorp.com>
2022-05-02 20:55:42 -0500

    feat: add dato to list of allowed next/image domains (#32)

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const withHashicorp = require('@hashicorp/platform-nextjs-plugin')
 module.exports = withHashicorp({
   dato: { token: 'dc45ff8c8b27dd22a7c24aaaf8aa75' },
   nextOptimizedImages: true,
-})()
+})({
+  images: {
+    domains: ['www.datocms-assets.com'],
+    disableStaticImages: true,
+  },
+})
