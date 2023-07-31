430a867fcff4a6084a75c1432a673e8703655344
Kendall Strautman <kendallstrautman@gmail.com>
2023-07-31 13:53:40 -0700

    format

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const withHashicorp = require('@hashicorp/platform-nextjs-plugin')
 
 module.exports = withHashicorp({
-  dato: { token: 'dc45ff8c8b27dd22a7c24aaaf8aa75' },
-  nextOptimizedImages: true,
+	dato: { token: 'dc45ff8c8b27dd22a7c24aaaf8aa75' },
+	nextOptimizedImages: true,
 })({
-  images: {
-    domains: ['www.datocms-assets.com'],
-    disableStaticImages: true,
-  },
+	images: {
+		domains: ['www.datocms-assets.com'],
+		disableStaticImages: true,
+	},
 })
