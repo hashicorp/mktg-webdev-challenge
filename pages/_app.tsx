5cb2bc524f45559494bc8ba484797b88b866da07
Kendall Strautman <kendallstrautman@gmail.com>
2023-07-31 10:02:14 -0700

    update deps remove platform-util

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import './style.css'
-import '@hashicorp/platform-util/nprogress/style.css'
-
-import NProgress from '@hashicorp/platform-util/nprogress'
-import Router from 'next/router'
-
-NProgress({ Router })
 
 const EmptyLayout = ({ children }) => children
 
