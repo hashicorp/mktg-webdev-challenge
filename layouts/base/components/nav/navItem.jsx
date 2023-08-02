785c4e786aaabe1b41d2577086675f56ce56c740
Kendall Strautman <kendallstrautman@gmail.com>
2023-08-02 12:22:26 -0700

    formatting

--- layouts/base/components/nav/navItem.jsx
+++ layouts/base/components/nav/navItem.jsx
@@ -5,34 +5,34 @@ import DropdownCaratMobile from './icons/icon-dropdown-mobile'
 import Promo from './promo'
 
 function NavItem({ active, handleActivate, children, promos, title }) {
-  const slug = slugify(title, { lower: true })
+	const slug = slugify(title, { lower: true })
 
-  return (
-    <li className={`nav-item ${slug}${active ? ' active' : ''}`}>
-      <button
-        type="button"
-        className="link"
-        onClick={() => handleActivate(slug)}
-      >
-        {title}
-        <DropdownCarat />
-        <DropdownCaratMobile />
-      </button>
-      <div className="panel">
-        <div className="breadcrumb" onClick={() => handleActivate('mobile')}>
-          <div className="g-grid-container">
-            <DropdownCaratMobile />
-            {title}
-          </div>
-        </div>
-        <div className="g-grid-container">
-          <div className="panel-content">{children}</div>
-          {promos &&
-            promos.map((promo) => <Promo key={promo.title} {...promo} />)}
-        </div>
-      </div>
-    </li>
-  )
+	return (
+		<li className={`nav-item ${slug}${active ? ' active' : ''}`}>
+			<button
+				type="button"
+				className="link"
+				onClick={() => handleActivate(slug)}
+			>
+				{title}
+				<DropdownCarat />
+				<DropdownCaratMobile />
+			</button>
+			<div className="panel">
+				<div className="breadcrumb" onClick={() => handleActivate('mobile')}>
+					<div className="g-grid-container">
+						<DropdownCaratMobile />
+						{title}
+					</div>
+				</div>
+				<div className="g-grid-container">
+					<div className="panel-content">{children}</div>
+					{promos &&
+						promos.map((promo) => <Promo key={promo.title} {...promo} />)}
+				</div>
+			</div>
+		</li>
+	)
 }
 
 export default NavItem
