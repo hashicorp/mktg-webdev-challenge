3af94e226769030f8344fa910e14440cb2dd879d
Chanh Nguyen <hello@chanh.design>
2023-11-21 17:00:23 -0700

    Create a base layout

--- pages/people/index.tsx
+++ pages/people/index.tsx
@@ -15,14 +15,67 @@ export default function PeoplePage({
 	allDepartments,
 }: Props): React.ReactElement {
 	return (
-		<main className="g-grid-container">
-			<h2>People Data</h2>
-			<pre className={style.myData}>{JSON.stringify(allPeople, null, 2)}</pre>
-			<h2>Departments Data</h2>
-			<pre className={style.myData}>
-				{JSON.stringify(allDepartments, null, 2)}
-			</pre>
-		</main>
+		<div>
+			<div>
+				{/* Filter */}
+				<div>Filter By Department</div>
+				<ul>
+					<li>
+						Category
+						<ul>
+							<li>Subcategory</li>
+							<ul>
+								<li>Subcategory</li>
+							</ul>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<main>
+				<div>
+					{/* Header */}
+					<div>
+						<h1>HashiCorp Humans</h1>
+						<p>Find a HashiCorp human</p>
+					</div>
+					{/* Search */}
+					<div>
+						<input type="search" name="filter" id="filter" />
+						<div>
+							<input
+								type="checkbox"
+								name="missing-people"
+								id="missing-people"
+								placeholder="Search people by name"
+							/>
+							<label htmlFor="missing-people">
+								Hide people missing a profile image
+							</label>
+						</div>
+					</div>
+				</div>
+				<div>
+					{/* Card */}
+					<ul>
+						<li>
+							<div>
+								<div>Avatar</div>
+								<h2>Name</h2>
+								<p>Title</p>
+								<p>Department</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+
+				<h2>People Data</h2>
+				<pre className={style.myData}>{JSON.stringify(allPeople, null, 2)}</pre>
+				<h2>Departments Data</h2>
+				<pre className={style.myData}>
+					{JSON.stringify(allDepartments, null, 2)}
+				</pre>
+			</main>
+		</div>
 	)
 }
 
