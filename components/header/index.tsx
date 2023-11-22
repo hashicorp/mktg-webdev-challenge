5b0abf212144bf3dd1362ea5c00bb10f94d33716
Chanh Nguyen <hello@chanh.design>
2023-11-21 17:37:22 -0700

    Style the header component

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import classNames from 'classnames'
+import style from './style.module.css'
 
 const Header = () => {
 	return (
-		<div>
-			<h1>HashiCorp Humans</h1>
-			<p>Find a HashiCorp human</p>
+		<div className={classNames(style.root)}>
+			<h1 className={classNames(style.heading)}>HashiCorp Humans</h1>
+			<p className={classNames(style.subheading)}>Find a HashiCorp human</p>
 		</div>
 	)
 }
